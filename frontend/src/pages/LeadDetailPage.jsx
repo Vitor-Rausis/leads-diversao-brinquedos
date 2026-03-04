ad270007503df038fd341b0b95d6c3916029e4d7
VitorRausis21 <vitor.rausis@innovo.com.br>
2026-03-04 09:59:29 -0300

    ui: exibe secoes de mensagens em coluna unica no detalhe do lead

--- frontend/src/pages/LeadDetailPage.jsx
+++ frontend/src/pages/LeadDetailPage.jsx
@@ -92,7 +92,7 @@ export default function LeadDetailPage() {
         )}
       </Card>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+      <div className="flex flex-col gap-6">
         {/* Mensagens Agendadas */}
         <Card className="p-6">
           <div className="flex items-center justify-between mb-4">
